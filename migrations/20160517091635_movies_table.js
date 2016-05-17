7ca03f4aaaa4c64113cc5cb1f01a655459101b97
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-17 10:15:48 -0600

    Changed the migration to only have the title and the UPC.

--- migrations/20160517091635_movies_table.js
+++ migrations/20160517091635_movies_table.js
@@ -3,20 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('movies', function(table) {
     table.increments();
     table.string('DVD_Title');
-    table.string('Studio');
-    table.string('Released');
-    table.string('Status');
-    table.string('Sound');
-    table.string('Versions');
-    table.float('Price');
-    table.string('Rating');
-    table.string('Year');
-    table.string('Genre');
-    table.string('Aspect');
     table.string('UPC');
-    table.dateTime('DVD_ReleaseDate');
-    table.integer('ID');
-    table.dateTime('Timestamp');
   });
 };
 
