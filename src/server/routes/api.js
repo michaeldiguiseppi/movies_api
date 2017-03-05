61d508cab64026036dc844ea5cc90f9ff3b45d2c
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2017-03-05 11:42:19 -0700

    Updated barcode scanning to clean data.

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -11,6 +11,9 @@ router.get('/:upc', getMovieByUpc);
 
 function getMovieByUpc(req, res, next) {
   var upc = req.params.upc;
+  if ((upc.length === 11|| upc.length === 13) && upc.charAt(0) === '0') {
+    upc = upc.slice(1, upc.length);
+  }
   Movies().select().where('UPC', upc).then(function (data) {
     res.json(data);
   }).catch(function(err) {
